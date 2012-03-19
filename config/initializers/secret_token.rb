40b3a14e2bdebfa07f44938c1a2d33172a7916be
Bob Van Landuyt <bob@10to1.be>
2012-03-19 20:39:45 +0100

    Initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ksatvlinderke::Application.config.secret_token = '8ce59578f97d2417942a8837d3674798385049c423ec6113dec1bc15fed8182a6bde063aed51d80edd26baacd5d5d8e7f62bbfeef8c7e8e22aae8de29a50b26d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ksatvlinderke::Application.config.session_store :cookie_store, key: '_ksatvlinderke_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ksatvlinderke::Application.config.session_store :active_record_store
